ef45f6fa4ef748c833a9b378d20913d859deec60
Nidhi123456N <125287004+Nidhi123456N@users.noreply.github.com>
2023-03-10 19:18:11 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,22 +1,62 @@
-  function validateForm() {
-  const name = document.getElementById("name").value;
-  const email = document.getElementById("email").value;
-  const password = document.getElementById("password").value;
-  const confirmPassword = document.getElementById("confirm-password").value;
+  // Initialize an empty array to store user entries
+let users = [];
 
-  // Check if all fields are filled out
-  if (name === "" || email === "" || password === "" || confirmPassword === "") {
-    alert("Please fill out all fields.");
-    return false;
-  }
+// Retrieve the form element from the DOM
+const form = document.getElementById("regForm");
 
-  // Check if password and confirm password match
-  if (password !== confirmPassword) {
-    alert("Passwords do not match.");
-    return false;
-  }
+// Retrieve the table element from the DOM
+const table = document.getElementById("userTable");
 
-  // If everything is filled out and passwords match, submit the form
-  alert("Registration successful!");
-  return true;
+// Define a function to add a user entry to the table
+function addUserToTable(user) {
+	// Create a new row element
+	const row = document.createElement("tr");
+	
+	// Add cells to the row for each user property
+	const nameCell = document.createElement("td");
+	nameCell.textContent = user.name;
+	row.appendChild(nameCell);
+	
+	const emailCell = document.createElement("td");
+	emailCell.textContent = user.email;
+	row.appendChild(emailCell);
+	
+	const passwordCell = document.createElement("td");
+	passwordCell.textContent = user.password;
+	row.appendChild(passwordCell);
+	
+	const dobCell = document.createElement("td");
+	dobCell.textContent = user.dob;
+	row.appendChild(dobCell);
+	
+	const termsCell = document.createElement("td");
+	termsCell.textContent = user.terms ? "Yes" : "No";
+	row.appendChild(termsCell);
+	
+	// Add the new row to the table
+	table.querySelector("tbody").appendChild(row);
 }
+
+// Define a function to validate the form before submission
+function validateForm() {
+	// Retrieve form fields from the DOM
+	const nameField = document.getElementById("name");
+	const emailField = document.getElementById("email");
+	const passwordField = document.getElementById("password");
+	const dobField = document.getElementById("dob");
+	const termsField = document.getElementById("terms");
+	
+	// Retrieve form field values
+	const name = nameField.value.trim();
+	const email = emailField.value.trim();
+	const password = passwordField.value.trim();
+	const dob = new Date(dobField.value);
+	const terms = termsField.checked;
+	
+	// Validate email address
+	if (!isValidEmail(email)) {
+		alert("Invalid email address");
+		return false;
+	}
+	
+	// Validate age
