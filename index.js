92238258a7fb0798d7f7993997372027e85addf6
Nidhi123456N <125287004+Nidhi123456N@users.noreply.github.com>
2023-03-10 19:49:17 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,49 +1,61 @@
   function addEntry() {
-	// Get form values
-	var name = document.getElementById("name").value;
-	var email = document.getElementById("email").value;
-	var password = document.getElementById("password").value;
-	var dob = document.getElementById("dob").value;
-	var accepted = document.getElementById("accepted").checked;
-	
-	// Check for valid email
-	var emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-	if (!emailRegex.test(email)) {
-		alert("Invalid email address");
-		return false;
-	}
-	
-	// Check age
-	var today = new Date();
-	var birthDate = new Date(dob);
-	var age = today.getFullYear() - birthDate.getFullYear();
-	var monthDiff = today.getMonth() - birthDate.getMonth();
-	if (monthDiff < 0 || (monthDiff === 0 && today.getDate() < birthDate.getDate())) {
-	    age--;
-	}
-	if (age < 18 || age > 55) {
-		alert("You must be between 18 and 55 years old");
-		return false;
-	}
-	
-	// Create table row
-	var table = document.getElementById("entries");
-	var row = table.insertRow(-1);
-	var nameCell = row.insertCell(0);
-	var emailCell = row.insertCell(1);
-	var passwordCell = row.insertCell(2);
-	var dobCell = row.insertCell(3);
-	var acceptedCell = row.insertCell(4);
-	nameCell.innerHTML = name;
-	emailCell.innerHTML = email;
-	passwordCell.innerHTML = password;
-	dobCell.innerHTML = dob;
-	acceptedCell.innerHTML = accepted ? "Yes" : "No";
-	
-	// Clear form
-	document.getElementById("registrationForm").reset();
-	
-	return false;
+    // Get form values
+    var name = document.getElementById("name").value;
+    var email = document.getElementById("email").value;
+    var password = document.getElementById("password").value;
+    var dob = document.getElementById("dob").value;
+    var accepted = document.getElementById("accepted").checked;
+    
+    // Check for valid email
+    var emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (!emailRegex.test(email)) {
+        alert("Invalid email address");
+        return false;
+    }
+    
+    // Check age
+    var today = new Date();
+    var birthDate = new Date(dob);
+    var age = today.getFullYear() - birthDate.getFullYear();
+    var monthDiff = today.getMonth() - birthDate.getMonth();
+    if (monthDiff < 0 || (monthDiff === 0 && today.getDate() < birthDate.getDate())) {
+        age--;
+    }
+    if (age < 18 || age > 55) {
+        alert("You must be between 18 and 55 years old");
+        return false;
+    }
+    
+    // Create table row
+    var table = document.getElementById("entries");
+    var row = table.insertRow(-1);
+    var nameCell = row.insertCell(0);
+    var emailCell = row.insertCell(1);
+    var passwordCell = row.insertCell(2);
+    var dobCell = row.insertCell(3);
+    var acceptedCell = row.insertCell(4);
+    nameCell.innerHTML = name;
+    emailCell.innerHTML = email;
+    passwordCell.innerHTML = password;
+    dobCell.innerHTML = dob;
+    acceptedCell.innerHTML = accepted ? "Yes" : "No";
+    
+    // Save entries to local storage
+    var entries = JSON.parse(localStorage.getItem("entries")) || [];
+    var entry = {
+        name: name,
+        email: email,
+        password: password,
+        dob: dob,
+        accepted: accepted
+    };
+    entries.push(entry);
+    localStorage.setItem("entries", JSON.stringify(entries));
+    
+    // Clear form
+    document.getElementById("registrationForm").reset();
+    
+    return false;
 }
 
 // Add event listener to form submit
@@ -52,4 +64,19 @@ form.addEventListener("submit", addEntry);
 
 // Load saved entries from local storage
 if (localStorage.getItem("entries")) {
-	var entries = JSON.parse(localStorage.getItem("entries"));
+    var entries = JSON.parse(localStorage.getItem("entries"));
+    var table = document.getElementById("entries");
+    for (var i = 0; i < entries.length; i++) {
+        var row = table.insertRow(-1);
+        var nameCell = row.insertCell(0);
+        var emailCell = row.insertCell(1);
+        var passwordCell = row.insertCell(2);
+        var dobCell = row.insertCell(3);
+        var acceptedCell = row.insertCell(4);
+        nameCell.innerHTML = entries[i].name;
+        emailCell.innerHTML = entries[i].email;
+        passwordCell.innerHTML = entries[i].password;
+        dobCell.innerHTML = entries[i].dob;
+        acceptedCell.innerHTML = entries[i].accepted ? "Yes" : "No";
+    }
+}
