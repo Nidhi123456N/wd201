ea72249e6fc07087bdebdee795e8d2f9db32667c
Nidhi123456N <125287004+Nidhi123456N@users.noreply.github.com>
2023-03-11 13:37:26 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,57 +1,25 @@
-  function addEntry() {
-    // Get form values
-    var name = document.getElementById("name").value;
-    var email = document.getElementById("email").value;
-    var password = document.getElementById("password").value;
-    var dob = document.getElementById("dob").value;
-    var accepted = document.getElementById("accepted").checked;
-    
-    // Check for valid email
-    var emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-    if (!emailRegex.test(email)) {
-        alert("Invalid email address");
-        return false;
-    }
-    
-    // Create table row
-    var table = document.getElementById("entries").getElementsByTagName("tbody")[0];
-    var row = table.insertRow(-1);
-    var nameCell = row.insertCell(0);
-    var emailCell = row.insertCell(1);
-    var passwordCell = row.insertCell(2);
-    var dobCell = row.insertCell(3);
-    var acceptedCell = row.insertCell(4);
-    nameCell.innerHTML = name;
-    emailCell.innerHTML = email;
-    passwordCell.innerHTML = password;
-    dobCell.innerHTML = dob;
-    acceptedCell.innerHTML = accepted ? "Yes" : "No";
-    
-    // Clear form
-    document.getElementById("registrationForm").reset();
-    
-    return false;
-}
+ const form = document.getElementById('registration-form');
+const tableBody = document.querySelector('table tbody');
 
-// Add event listener to form submit
-var form = document.getElementById("registrationForm");
-form.addEventListener("submit", addEntry);
-
-// Load saved entries from local storage
-if (localStorage.getItem("entries")) {
-    var entries = JSON.parse(localStorage.getItem("entries"));
-    var table = document.getElementById("entries").getElementsByTagName("tbody")[0];
-    for (var i = 0; i < entries.length; i++) {
-        var row = table.insertRow(-1);
-        var nameCell = row.insertCell(0);
-        var emailCell = row.insertCell(1);
-        var passwordCell = row.insertCell(2);
-        var dobCell = row.insertCell(3);
-        var acceptedCell = row.insertCell(4);
-        nameCell.innerHTML = entries[i].name;
-        emailCell.innerHTML = entries[i].email;
-        passwordCell.innerHTML = entries[i].password;
-        dobCell.innerHTML = entries[i].dob;
-        acceptedCell.innerHTML = entries[i].accepted ? "Yes" : "No";
-    }
-}
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+  
+  const name = document.getElementById('name').value;
+  const email = document.getElementById('email').value;
+  const password = document.getElementById('password').value;
+  const dob = document.getElementById('dob').value;
+  const acceptedTerms = document.getElementById('accepted-terms').checked;
+  
+  const newRow = document.createElement('tr');
+  newRow.innerHTML = `
+    <td>${name}</td>
+    <td>${email}</td>
+    <td>${password}</td>
+    <td>${dob}</td>
+    <td>${acceptedTerms ? 'Yes' : 'No'}</td>
+  `;
+  
+  tableBody.appendChild(newRow);
+  
+  form.reset();
+});
