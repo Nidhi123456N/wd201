eb68764cb24f045b64156732f0d5e028599ad30c
Nidhi123456N <125287004+Nidhi123456N@users.noreply.github.com>
2023-03-11 16:07:53 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,58 +1,53 @@
-// Get the registration form and the table body
-const registrationForm = document.getElementById("registration-form");
-const tableBody = document.querySelector("#registered-users tbody");
+const form = document.querySelector('#registration-form');
+const usersTable = document.querySelector('#users-table tbody');
 
-// Add an event listener to the registration form
-registrationForm.addEventListener("submit", function (event) {
-  event.preventDefault(); // Prevent the form from submitting
+// Load previously saved data from web storage
+const savedData = JSON.parse(localStorage.getItem('registrationData'));
+if (savedData) {
+  savedData.forEach(user => {
+    addUserToTable(user);
+  });
+}
 
-  // Get the values from the form
-  const name = document.getElementById("name").value;
-  const email = document.getElementById("email").value;
-  const password = document.getElementById("password").value;
-  const dob = new Date(document.getElementById("dob").value);
-  const acceptTerms = document.getElementById("accept-terms").checked;
+// Add a new user to the table
+function addUserToTable(user) {
+  const row = document.createElement('tr');
+  row.innerHTML = `
+    <td>${user.name}</td>
+    <td>${user.email}</td>
+    <td>${user.password}</td>
+    <td>${user.dob}</td>
+    <td>${user.acceptTerms ? 'Yes' : 'No'}</td>
+  `;
+  usersTable.appendChild(row);
+}
 
-  // Check if the user is between 18 and 55 years old
-  const today = new Date();
-  const age = today.getFullYear() - dob.getFullYear();
-  const monthDiff = today.getMonth() - dob.getMonth();
-  if (
-    monthDiff < 0 ||
-    (monthDiff === 0 && today.getDate() < dob.getDate())
-  ) {
-    age--;
-  }
+// Validate date of birth
+function isValidDob(dob) {
+  const currentDate = new Date();
+  const maxAge = 55;
+  const minAge = 18;
+  const minDob = new Date(currentDate.getFullYear() - maxAge, currentDate.getMonth(), currentDate.getDate());
+  const maxDob = new Date(currentDate.getFullYear() - minAge, currentDate.getMonth(), currentDate.getDate());
+  dob = new Date(dob);
+  return dob >= minDob && dob <= maxDob;
+}
 
-  if (age < 18 || age > 55) {
-    alert("Sorry, you must be between 18 and 55 years old to register.");
+// Handle form submission
+form.addEventListener('submit', event => {
+  event.preventDefault();
+  const name = form.elements['name'].value.trim();
+  const email = form.elements['email'].value.trim();
+  const password = form.elements['password'].value.trim();
+  const dob = form.elements['dob'].value.trim();
+  const acceptTerms = form.elements['accept-terms'].checked;
+  if (!isValidDob(dob)) {
+    alert('You must be between 18 and 55 years old to register.');
     return;
   }
-
-  // Create a new table row and cells
-  const newRow = tableBody.insertRow(-1);
-  const nameCell = newRow.insertCell(0);
-  const emailCell = newRow.insertCell(1);
-  const passwordCell = newRow.insertCell(2);
-  const dobCell = newRow.insertCell(3);
-  const acceptTermsCell = newRow.insertCell(4);
-
-  // Add the values to the cells
-  nameCell.textContent = name;
-  emailCell.textContent = email;
-  passwordCell.textContent = password;
-  dobCell.textContent = dob.toLocaleDateString();
-  acceptTermsCell.textContent = acceptTerms ? "Yes" : "No";
-
-  // Clear the form
-  registrationForm.reset();
-
-  // Save the data to web storage
-  saveDataToWebStorage();
-});
-
-// Load the data from web storage when the page loads
-window.addEventListener("load", function () {
-  loadDataFromWebStorage();
+  const user = { name, email, password, dob, acceptTerms };
+  addUserToTable(user);
+  form.reset();
+  localStorage.setItem('registrationData', JSON.stringify([...(JSON.parse(localStorage.getItem('registrationData')) || []), user]));
 });
 
